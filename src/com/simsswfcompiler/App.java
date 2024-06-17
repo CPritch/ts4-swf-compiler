6d3bc62c69147968ab6389e94fa298eafc080254
CPritch <charlespritchard.work@gmail.com>
2024-06-18 00:00:04 +0100

    feat: initial project

---
+++ src/com/simsswfcompiler/App.java
@@ -0,0 +1,11 @@
+package com.simsswfcompiler;
+
+public class App {
+	private static final String APP_NAME = "Sims SWF Editor";
+	
+	public static void main(String[] args) {
+		System.out.println(String.format("%s started.", App.APP_NAME));
+		System.out.println(String.format("%s ended.", App.APP_NAME));
+	}
+
+}
