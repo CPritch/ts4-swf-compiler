95888a7114de822c46e871bc8f797ce7dc91b7dd
CPritch <charlespritchard.work@gmail.com>
2024-06-20 01:58:17 +0100

    feat: slight refactor and verbose messaging

--- src/com/simsswfcompiler/App.java
+++ src/com/simsswfcompiler/App.java
@@ -1,11 +1,4 @@
 package com.simsswfcompiler;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -14,24 +7,36 @@ import com.github.fge.jsonschema.core.report.ProcessingMessage;
 import com.github.fge.jsonschema.core.report.ProcessingReport;
 import com.github.fge.jsonschema.main.JsonSchema;
 import com.github.fge.jsonschema.main.JsonSchemaFactory;
-
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 @Command(name = "Sims SWF Compiler", mixinStandardHelpOptions = true, version = "0.1-alpha",
-description = "Processes SWF files.")
+        description = "Processes SWF files.")
 public class App implements Runnable {
-	@Option(names = {"-src"}, description = "The folder containing assets to import.", required = true)
-    private String src;
-	
-	@Option(names = {"-dst"}, description = "The SWF file location.", required = true)
-    private String dst;
-	
-	@Option(names = {"-out"}, description = "The filepath for saving the modified SWF.", required = true)
-    private String out;
-	
-	private SimsSWF simsSwf;
+    @Option(names = {"-src"}, description = "The folder containing assets to import.", required = true)
+    private String srcDir;
+
+    @Option(names = {"-dst"}, description = "The SWF file location.", required = true)
+    private String dstFile;
+
+    @Option(names = {"-out"}, description = "The filepath for saving the modified SWF.", required = true)
+    private String outFile;
+
+    @Option(names = {"-verbose"}, description = "Enable verbose logging.")
+    private boolean verbose;
+
+    private SimsSWF simsSwf;
+    private ObjectMapper objectMapper;
+    private JsonSchema jsonSchema;
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
@@ -40,88 +45,96 @@ public class App implements Runnable {
 
     @Override
     public void run() {
-        System.out.println(String.format("Decompiling SWF: %s.", dst));
-        simsSwf = new SimsSWF(dst);
-        modifyActionScript();
-        modifyTags();
-        simsSwf.saveTo(out);
-        System.out.println(String.format("Modified SWF saved to %s", out));
-    }
-    
-    private void modifyActionScript() {
-    	// Read .as files from src/scripts
-      Path scriptsDir = Paths.get(src, "scripts");
-      try (Stream<Path> paths = Files.walk(scriptsDir)) {
-          paths
-              .filter(Files::isRegularFile)
-              .filter(path -> path.toString().endsWith(".as"))
-              .forEach(path -> {
-                  String fileName = path.getFileName().toString();
-                  String fileContent = "";
-                  try {
-                      fileContent = Files.readString(path);
-                  } catch (IOException e) {
-                      System.err.println("Error reading file: " + fileName);
-                      e.printStackTrace();
-                  }
-                  simsSwf.addAS3(fileName, fileContent);
-              });
-      } catch (IOException e) {
-          System.err.println("Error walking through scripts directory.");
-          e.printStackTrace();
-      }
-    }
-    
-    private void modifyTags() {
-        Path tagsDir = Paths.get(src, "tags");
-        try (Stream<Path> paths = Files.walk(tagsDir)) {
-            paths
-                .filter(Files::isRegularFile)
-                .filter(path -> path.toString().endsWith(".json") || path.toString().endsWith(".yml"))
-                .forEach(this::processTagFile);
-        } catch (IOException e) {
-            System.err.println("Error walking through tags directory.");
-            e.printStackTrace();
+        try {
+            initializeComponents();
+            logMessage(String.format("Decompiling SWF: %s.", dstFile));
+            simsSwf = new SimsSWF(Paths.get(dstFile));
+            modifyActionScript();
+            modifyTags();
+            simsSwf.saveTo(Paths.get(outFile));
+            logMessage(String.format("Modified SWF saved to %s", outFile));
+        } catch (IOException | ProcessingException e) {
+            logError("Error occurred during processing: " + e.getMessage());
         }
     }
-    
-    private void processTagFile(Path tagFile) {
-    	try {
-    		ObjectMapper objectMapper = new ObjectMapper();
-    	    InputStream inputStream = getClass().getClassLoader().getResourceAsStream("TagModel.json");
-    	    String tagModel = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
-    	    
-    	    JsonNode tagNode = objectMapper.readTree(Files.readString(tagFile));
-    		JsonNode schemaNode = objectMapper.readTree(tagModel);
-    		
-    		JsonSchemaFactory schemaFactory = JsonSchemaFactory.byDefault();
-    		JsonSchema schema = schemaFactory.getJsonSchema(schemaNode);
-    		ProcessingReport report = schema.validate(tagNode);
-    		if (report.isSuccess()) {
-			   System.out.println(String.format("JSON config for %s is valid.", tagNode.get("instanceName")));
-			   String srcInstanceName = tagNode.get("copy").get("instanceName").asText();
-			   String srcInstanceType = tagNode.get("copy").get("instanceName").asText();
-			   String dstInstanceName = tagNode.get("instanceName").asText();
-			   String dstClassName = tagNode.get("className") != null ? tagNode.get("className").asText() : null;
-			   simsSwf.copyTag(srcInstanceName, srcInstanceType, dstInstanceName, dstClassName);
-    		} else {
-			    System.err.println("JSON config is invalid for: " + tagFile);
-			    for (ProcessingMessage item : report) {
-			    	System.err.println(item.getMessage());
-			    }
-			    System.exit(1);
-			}   
-    		
-    	} catch (IOException e) {
-            System.err.println("Error reading tag file: " + tagFile.getFileName());
-            System.err.println(e.toString());
-            e.printStackTrace();
-            System.exit(1);
-        } catch (ProcessingException e) {
-			System.err.println("Error processing JSON schema: " + e.toString());
-			e.printStackTrace();
-			System.exit(1);
-		}
 
+    private void initializeComponents() throws IOException, ProcessingException {
+        objectMapper = new ObjectMapper();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("TagModel.json");
+        String tagModel = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+        JsonNode schemaNode = objectMapper.readTree(tagModel);
+        JsonSchemaFactory schemaFactory = JsonSchemaFactory.byDefault();
+        jsonSchema = schemaFactory.getJsonSchema(schemaNode);
+    }
+
+    private void modifyActionScript() {
+        Path scriptsDir = Paths.get(srcDir, "scripts");
+        try (Stream<Path> paths = Files.walk(scriptsDir)) {
+            paths
+                    .filter(Files::isRegularFile)
+                    .filter(path -> path.toString().endsWith(".as"))
+                    .forEach(path -> {
+                        String fileName = path.getFileName().toString();
+                        String fileContent = "";
+                        try {
+                            fileContent = Files.readString(path);
+                        } catch (IOException e) {
+                            logError("Error reading file: " + fileName + " - " + e.getMessage());
+                        }
+                        simsSwf.addAS3(fileName, fileContent);
+                    });
+        } catch (IOException e) {
+            logError("Error walking through scripts directory: " + e.getMessage());
+        }
+    }
+
+    private void modifyTags() {
+        Path tagsDir = Paths.get(srcDir, "tags");
+        try (Stream<Path> paths = Files.walk(tagsDir)) {
+            paths
+                    .filter(Files::isRegularFile)
+                    .filter(path -> path.toString().endsWith(".json") || path.toString().endsWith(".yml"))
+                    .forEach(this::processTagFile);
+        } catch (IOException e) {
+            logError("Error walking through tags directory: " + e.getMessage());
+        }
+    }
+
+    private void processTagFile(Path tagFile) {
+        try {
+            JsonNode tagNode = objectMapper.readTree(Files.readString(tagFile));
+            ProcessingReport report = jsonSchema.validate(tagNode);
+            if (report.isSuccess()) {
+                logInfo(String.format("JSON config for %s is valid.", tagNode.get("instanceName")));
+                String srcInstanceName = tagNode.get("copy").get("instanceName").asText();
+                String srcInstanceType = tagNode.get("copy").get("type").asText();
+                String dstInstanceName = tagNode.get("instanceName").asText();
+                String dstClassName = tagNode.get("className") != null ? tagNode.get("className").asText() : null;
+                simsSwf.copyTag(srcInstanceName, srcInstanceType, dstInstanceName, dstClassName);
+            } else {
+                logError("JSON config is invalid for: " + tagFile);
+                for (ProcessingMessage item : report) {
+                    logError(item.getMessage());
+                }
+            }
+        } catch (IOException e) {
+            logError("Error reading tag file: " + tagFile.getFileName() + " - " + e.getMessage());
+        } catch (ProcessingException e) {
+        	logError("Error processing tag json: " + tagFile.getFileName() + " - " + e.getMessage());
+		}
+    }
+    
+    private void logMessage(String message) {
+    	System.out.println(message);
+    }
+
+    private void logInfo(String message) {
+        if (verbose) {
+        	logMessage(message);
+        }
+    }
+
+    private void logError(String message) {
+        System.err.println(message);
     }
 }
--- src/com/simsswfcompiler/SimsSWF.java
+++ src/com/simsswfcompiler/SimsSWF.java
@@ -1,188 +1,156 @@
 package com.simsswfcompiler;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import com.jpexs.decompiler.flash.ReadOnlyTagList;
 import com.jpexs.decompiler.flash.SWF;
 import com.jpexs.decompiler.flash.SwfOpenException;
 import com.jpexs.decompiler.flash.abc.ScriptPack;
-import com.jpexs.decompiler.flash.configuration.Configuration;
-import com.jpexs.decompiler.flash.flexsdk.MxmlcAs3ScriptReplacer;
 import com.jpexs.decompiler.flash.gui.Main;
 import com.jpexs.decompiler.flash.importers.As3ScriptReplaceException;
 import com.jpexs.decompiler.flash.importers.As3ScriptReplaceExceptionItem;
 import com.jpexs.decompiler.flash.importers.As3ScriptReplacerFactory;
 import com.jpexs.decompiler.flash.importers.As3ScriptReplacerInterface;
-import com.jpexs.decompiler.flash.importers.FFDecAs3ScriptReplacer;
 import com.jpexs.decompiler.flash.tags.Tag;
 import com.jpexs.decompiler.flash.tags.base.PlaceObjectTypeTag;
 import com.jpexs.decompiler.flash.timeline.Timelined;
 
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SimsSWF {
-    
     private SWF swf;
     private List<ScriptPack> packs;
-    
-    public SimsSWF(String filePath) {
-        try (FileInputStream fis = new FileInputStream(filePath)) {
+
+    public SimsSWF(Path filePath) {
+        try (FileInputStream fis = new FileInputStream(filePath.toFile())) {
             this.swf = new SWF(fis, true);
-            this.packs = swf.getAS3Packs();            
-        } catch(SwfOpenException ex) {
-        	ex.printStackTrace();
-            System.err.println("Error opening swf: " + ex.toString());
-            System.exit(1);
+            this.packs = swf.getAS3Packs();
+        } catch (SwfOpenException ex) {
+            throw new RuntimeException("Error opening SWF: " + ex.getMessage(), ex);
         } catch (IOException | InterruptedException ex) {
-            ex.printStackTrace();
-            System.err.println("Error reading swf file: " + ex.toString());
-            System.exit(1);
+            throw new RuntimeException("Error reading SWF file: " + ex.getMessage(), ex);
         }
     }
-    
-    public void saveTo(String filePath) {
-    	if (swf == null) {
-        	System.err.println("SWF not open");
-        	System.exit(1);
+
+    public void saveTo(Path filePath) {
+        if (swf == null) {
+            throw new IllegalStateException("SWF not open");
         }
-    	
-    	File outFile = new File(filePath);
-    	try {
-            try (OutputStream fos = new BufferedOutputStream(new FileOutputStream(outFile))) {
-                swf.saveTo(fos);
-            }
+
+        try (OutputStream fos = new BufferedOutputStream(new FileOutputStream(filePath.toFile()))) {
+            swf.saveTo(fos);
         } catch (IOException e) {
-            System.err.println("Error writing: " + e.toString());
-            System.exit(1);
+            throw new RuntimeException("Error writing SWF: " + e.getMessage(), e);
         }
     }
-    
+
     public void copyTag(String srcInstanceName, String srcTagType, String dstInstanceName, String dstClassName) {
-		List<Tag> foundTags = findTag(swf, srcInstanceName);
-		if (foundTags.size() == 0) {
-			System.err.println("Error: could not find tag with name: " + srcInstanceName);
-			System.exit(1);
-		}
-		if (foundTags.size() > 1) {
-			System.err.println(
-				String.format("Error: found %s tags for %s, please be more specific", foundTags.size(), srcInstanceName)
-			);
-			System.exit(1);
-		}
-		PlaceObjectTypeTag foundTag = (PlaceObjectTypeTag)foundTags.getFirst();
-		System.out.println("Found matching tag: " + foundTag.getName());
-		try {
-			PlaceObjectTypeTag copiedTag = (PlaceObjectTypeTag)foundTag.cloneTag();
-			copiedTag.setInstanceName(dstInstanceName);
-			if (dstClassName != null && dstClassName.length() > 0) {
-				copiedTag.setClassName(dstClassName);
-			}
-			copiedTag.setModified(true);
-			Timelined timeline = foundTag.getTimelined();
-			timeline.addTag(timeline.indexOfTag(foundTag) + 1, copiedTag);
-			System.out.println("Created new tag: " + copiedTag.getInstanceName());
-		} catch(IOException | InterruptedException ex) {
-			System.err.println("Error copying tag: " + ex.toString());
-		}
+        List<Tag> foundTags = findTag(swf, srcInstanceName);
+        if (foundTags.isEmpty()) {
+            throw new IllegalArgumentException("Could not find tag with name: " + srcInstanceName);
+        }
+        if (foundTags.size() > 1) {
+            throw new IllegalArgumentException(String.format("Found %s tags for %s, please be more specific", foundTags.size(), srcInstanceName));
+        }
+
+        PlaceObjectTypeTag foundTag = (PlaceObjectTypeTag) foundTags.get(0);
+        try {
+            PlaceObjectTypeTag copiedTag = (PlaceObjectTypeTag) foundTag.cloneTag();
+            copiedTag.setInstanceName(dstInstanceName);
+            if (dstClassName != null && !dstClassName.isEmpty()) {
+                copiedTag.setClassName(dstClassName);
+            }
+            copiedTag.setModified(true);
+            Timelined timeline = foundTag.getTimelined();
+            timeline.addTag(timeline.indexOfTag(foundTag) + 1, copiedTag);
+        } catch (IOException | InterruptedException ex) {
+            throw new RuntimeException("Error copying tag: " + ex.getMessage(), ex);
+        }
     }
-    
+
     public List<Tag> findTag(Timelined parent, String name) {
-    	ReadOnlyTagList tags = parent.getTags();
-    	List<Tag> foundTags = new ArrayList<Tag>();
-    	for (Tag t : tags) {
-    		if (t instanceof Timelined) {
-    			foundTags.addAll(findTag((Timelined) t, name));
-    		} else if(t instanceof PlaceObjectTypeTag) {
-    			PlaceObjectTypeTag pt = (PlaceObjectTypeTag) t;
-    			String tagInstanceName = pt.getInstanceName();
-    			if (tagInstanceName != null && tagInstanceName.contains(name)) {
-    				foundTags.add(pt);
-    			}
-    		}
-    	}
-    	return foundTags;
+        List<Tag> foundTags = new ArrayList<>();
+        ReadOnlyTagList tags = parent.getTags();
+        for (Tag tag : tags) {
+            if (tag instanceof Timelined) {
+                foundTags.addAll(findTag((Timelined) tag, name));
+            } else if (tag instanceof PlaceObjectTypeTag) {
+                PlaceObjectTypeTag placedTag = (PlaceObjectTypeTag) tag;
+                String tagInstanceName = placedTag.getInstanceName();
+                if (tagInstanceName != null && tagInstanceName.contains(name)) {
+                    foundTags.add(placedTag);
+                }
+            }
+        }
+        return foundTags;
     }
-    
+
     public void addAS3(String fileName, String fileContent) {
         if (swf == null) {
-        	System.err.println("Error: SWF not open");
-        	System.exit(1);
+            throw new IllegalStateException("SWF not open");
         }
         if (packs == null) {
-        	System.err.println("Error: AS3 not decompiled");
-        	System.exit(1);
+            throw new IllegalStateException("AS3 not decompiled");
         }
+
         String className = getClassNameFromFileContent(fileContent);
         boolean found = false;
         for (ScriptPack pack : packs) {
-        	if (pack.getClassPath().toString().equals(className)) {
-        		found = true;
-        		System.out.println(String.format("Replacing %s with %s", pack.getClassPath().toString(), fileName));
-        		replaceAS3(fileContent, pack);
-        	}
+            if (pack.getClassPath().toString().equals(className)) {
+                found = true;
+                replaceAS3(fileContent, pack);
+            }
         }
-        if (found == false) {
-        	System.err.println(
-    			String.format("Error: Could not find a matching class for %s. Adding new AS3 is unsupported for now.", className)
-        	);
-        	System.exit(1);
+
+        if (!found) {
+            throw new IllegalArgumentException(String.format("Could not find a matching class for %s. Adding new AS3 is unsupported for now.", className));
         }
     }
-    
+
     private static String getClassNameFromFileContent(String fileContent) {
-    	Pattern packagePattern = Pattern.compile("package\\s+([\\w\\.]+)\\s*\\{");
+        Pattern packagePattern = Pattern.compile("package\\s+([\\w\\.]+)\\s*\\{");
         Pattern classPattern = Pattern.compile("public\\s+(final\\s+)?class\\s+(\\w+)");
 
         Matcher packageMatcher = packagePattern.matcher(fileContent);
         Matcher classMatcher = classPattern.matcher(fileContent);
 
         if (packageMatcher.find() && classMatcher.find()) {
-        	return packageMatcher.group(1) + "." + classMatcher.group(2);
+            return packageMatcher.group(1) + "." + classMatcher.group(2);
         }
-        throw new Error("ClassName could not be parsed");
+        throw new IllegalArgumentException("ClassName could not be parsed");
     }
-    
-    private static void replaceAS3(String fileContent, ScriptPack pack) {
-    	As3ScriptReplacerInterface scriptReplacer = As3ScriptReplacerFactory.createByConfig(false);
-    	if (!scriptReplacer.isAvailable()) {
-            System.err.println("Error: Current script replacer is not available.");
-            if (scriptReplacer instanceof FFDecAs3ScriptReplacer) {
-                System.err.println("Current replacer: FFDec");
-                final String adobePage = "http://www.adobe.com/support/flashplayer/downloads.html";
-                System.err.println("For ActionScript 3 direct editation, a library called \"PlayerGlobal.swc\" needs to be downloaded from Adobe homepage:");
-                System.err.println(adobePage);
-                System.err.println("Download the library called PlayerGlobal(.swc), and place it to directory");
-                System.err.println(Configuration.getFlashLibPath().getAbsolutePath());
-            } else if (scriptReplacer instanceof MxmlcAs3ScriptReplacer) {
-                System.err.println("Current replacer: Flex SDK");
-                final String flexPage = "http://www.adobe.com/devnet/flex/flex-sdk-download.html";
-                System.err.println("For ActionScript 3 direct editation, Flex SDK needs to be download");
-                System.err.println(flexPage);
-                System.err.println("Download FLEX Sdk, unzip it to some directory and set its directory path in the configuration");
-            }
-            System.exit(1);
+
+    private void replaceAS3(String fileContent, ScriptPack pack) {
+        As3ScriptReplacerInterface scriptReplacer = As3ScriptReplacerFactory.createByConfig(false);
+        if (!scriptReplacer.isAvailable()) {
+            throw new IllegalStateException("Current script replacer is not available.");
         }
-    	
-    	try {
+
+        try {
             pack.abc.replaceScriptPack(scriptReplacer, pack, fileContent, Main.getDependencies(pack.abc.getSwf()));
         } catch (As3ScriptReplaceException asre) {
             for (As3ScriptReplaceExceptionItem item : asre.getExceptionItems()) {
-                String r = "%error% on line %line%, column %col%, file: %file%".replace("%error%", "" + item.getMessage());
-                r = r.replace("%line%", Long.toString(item.getLine()));
-                r = r.replace("%file%", "" + item.getFile());
-                r = r.replace("%col%", "" + item.getCol());
-                System.err.println(r);
+                System.err.println(formatErrorMessage(item));
             }
-            System.exit(1);
+            throw new RuntimeException("Error replacing ActionScript 3 script", asre);
         } catch (InterruptedException | IOException ex) {
-        	System.err.println("Error getting script dependencies: " + ex.toString());
+            throw new RuntimeException("Error getting script dependencies: " + ex.getMessage(), ex);
         }
+    }
+
+    private static String formatErrorMessage(As3ScriptReplaceExceptionItem item) {
+        String message = "%error% on line %line%, column %col%, file: %file%";
+        message = message.replace("%error%", item.getMessage());
+        message = message.replace("%line%", Long.toString(item.getLine()));
+        message = message.replace("%file%", item.getFile());
+        message = message.replace("%col%", Long.toString(item.getCol()));
+        return message;
     }
 }
